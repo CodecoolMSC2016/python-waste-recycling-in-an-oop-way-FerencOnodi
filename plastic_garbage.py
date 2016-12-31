9bb4359117493dbcab2a36147878419ce0c54ac2
FerencOnodi <onodiferenc91@gmail.com>
2016-12-31 14:25:25 +0100

    Implemented init and cleaned methods

--- plastic_garbage.py
+++ plastic_garbage.py
@@ -2,4 +2,12 @@ from garbage import Garbage
 
 
 class PlasticGarbage(Garbage):
-    pass
+
+    def __init__(self, name, is_clean):
+        self.name = name
+        self.is_clean = is_clean
+
+    def cleaned(self):
+        if self.is_clean == False:
+            self.is_clean = True
+        return self.is_clean
